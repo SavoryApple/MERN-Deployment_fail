f23d48fa88866601069d087fcca319d2b184fc19
Seann Avery <SeannAvery@gmail.com>
2022-03-23 15:57:37 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import PirateList from './components/PirateList';
+import Update from './views/Update';
+import Create from './views/Create';
+import PirateAbout from './components/PirateAbout';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    //localhost:3000
+    <BrowserRouter>
+      <div className="App">
+        <Route exact path="/pirate/:id">
+          <PirateAbout />
+          {/* <Update/> */}
+        </Route>
+        <Route exact path="/new/pirate">
+          <Create/>
+        </Route>
+        <Route exact path="/pirates">
+          <PirateList />
+        </Route>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
